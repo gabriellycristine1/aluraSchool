c57ca2192033cef1d83d53ab1fd2d21407f69faa
gabriellycristine1 <gabrielly@BOOK-KF0DTIRG00>
2024-06-23 00:47:55 -0400

    implementing more console logging for clearer prints

--- scripts/fusion_shop_fev.py
+++ scripts/fusion_shop_fev.py
@@ -7,12 +7,17 @@ path_csv = 'data_raw/dados_empresaB.csv'
 # Extract 
 
 data_companyA = Data.read_data(path_json, 'json')
-print(f'lendo dados empresa A: {data_companyA.name_columns}')
-print(F'Quantidade de linhas nos dados Empresa A: {data_companyA.qtd_lines}')
+print(160*'=')
+print(f'|  Reading Company A data: {data_companyA.name_columns}{28*" "}{"|"}') 
+print(160*'=')
+print(F'|  Number of rows in Company A data: {data_companyA.qtd_lines}{117*" "}{"|"}')
+print(160*'=')
 
 data_companyB = Data.read_data(path_csv, 'csv')
-print(f'lendo dados empresa B:{data_companyB.name_columns}')
-print(F'Quantidade de linhas nos dados Empresa B: {data_companyB.qtd_lines}')
+print(f'|  Reading Company B data:{data_companyB.name_columns}{7*" "}{"|"}')
+print(160*'=')
+print(F'|  Number of rows in Company B data: {data_companyB.qtd_lines}{117*" "}{"|"}')
+print(160*'=')
 
 # Transform 
 
@@ -24,14 +29,18 @@ key_mapping = {'Nome do Item': 'Nome do Produto',
                 'Data da Venda': 'Data da Venda'}
 
 data_companyB.rename_columns(key_mapping)
-print(f'padronizando nome das colunas: {data_companyB.name_columns}')
+print(f'| Standardizing column names B: {data_companyB.name_columns}{6*" "}{"|"}')
+print(160*'=')
 
 data_fusion = Data.join_datas(data_companyA,data_companyB)
-print(f'lendo dados pós fusão: {data_fusion.name_columns}')
-print(F'Quantidade de linhas nos dados pós fusão: {data_fusion.qtd_lines}')
+print(f'| reading data after data fusion: {data_fusion.name_columns}{4*" "}{"|"}')
+print(160*'=')
+print(F'| Number of lines in the data after fusion: {data_fusion.qtd_lines}{110*" "}{"|"}')
+print(160*'=')
 
 #Load
 path_data_combined = 'data_processed/dados_combinados.csv'
 
 data_fusion.saving_datas(path_data_combined)
-print(f'novos dados salvos no caminho: {path_data_combined}')
+print(f'| New data saved in the path: {path_data_combined}{93*" "}{"|"}')
+print(160*'=')
